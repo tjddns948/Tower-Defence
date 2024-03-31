a1b88ec925e21d02d333632c1cba475055aadb3d
tjddns948 <139138426+tjddns948@users.noreply.github.com>
2024-03-31 15:25:06 +0900

    set

--- Assets/CSW/Scripts/MonsterSpawn.cs
+++ Assets/CSW/Scripts/MonsterSpawn.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MonsterSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject monsterPrefab; //몬스터 프리팹
+    public Transform spawnPoint; //생성될 위치
+    public float spawnInterval = 1f; // 몬스터 스폰 간격
+    public int totalMonsterCount = 70; //총 몬스터 수
+
+    private int currentMonsterCount = 0; // 생성된 몬스터 수
+
     void Start()
     {
-        
+        InvokeRepeating("SpawnMonster", 0f, spawnInterval); // 일정간격으로 몬스터 생성 시작
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnMonster()
     {
-        
+        if(currentMonsterCount >= totalMonsterCount)
+        {
+            //몬스터 모두 생성하면 스폰 중지
+            CancelInvoke("SpawnMonster");
+            return;
+
+        }
+        //몬스터 생성
+        Instantiate(monsterPrefab, spawnPoint.position, spawnPoint.rotation);
+        currentMonsterCount++;
     }
+
 }
