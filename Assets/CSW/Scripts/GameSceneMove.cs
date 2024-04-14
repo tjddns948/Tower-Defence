f0aee05111d83602b3239638807626c2e9640b30
tjddns948 <139138426+tjddns948@users.noreply.github.com>
2024-04-14 10:45:53 +0900

    set

---
+++ Assets/CSW/Scripts/GameSceneMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameSceneMove : MonoBehaviour
+{
+    
+    public void GameSceneCtrl()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
