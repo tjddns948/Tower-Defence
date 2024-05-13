01d61d81c9368562048101e79003efcafda8f988
tjddns948 <139138426+tjddns948@users.noreply.github.com>
2024-05-13 14:38:14 +0900

    set

--- Assets/HSH/Player/Script/PlayerShot.cs
+++ Assets/HSH/Player/Script/PlayerShot.cs
@@ -3,32 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
 
 public class PlayerShot: MonoBehaviour
 {
     public GameObject bulletFactory;
+    public GameObject magicCircle;
     public Transform bulletSpawn;
-    public float fireRate = 0.1f; //  초당 발사되는 총알의 개수
+    public float fireRate = 0.1f; // 초당 발사되는 총알의 개수
     private float nextFireTime = 0f; // 다음 총알 발사 시간
-
+    Renderer magicCircleRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        magicCircleRenderer = magicCircle.GetComponent<Renderer>();
+        SetMagicCircleAlpha(0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0) && magicCircleRenderer.material.color.a <= 1)
+        {
+            StartCoroutine(FadeInMagicCircle());
+        }
         if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
         {
             OneShotFire();  
             nextFireTime = Time.time + 1f / fireRate;
         }
-
+        if (Input.GetMouseButtonUp(0) && magicCircleRenderer.material.color.a > 0)
+        {
+            StartCoroutine(FadeOutMagicCircle());
+        }
     }
-
 
     void OneShotFire()
     {
@@ -39,7 +48,34 @@ public class PlayerShot: MonoBehaviour
 
         // 총알의 방향을 플레이어가 바라보는 방향으로 설정
         bullet.transform.forward = shootDirection;
-
     }
 
+    IEnumerator FadeInMagicCircle()
+    {
+        float alpha = 0f;
+        while (alpha <= 1)
+        {
+            alpha += 0.1f;
+            SetMagicCircleAlpha(alpha);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    IEnumerator FadeOutMagicCircle()
+    {
+        float alpha = 1f;
+        while (alpha > 0)
+        {
+            alpha -= 0.1f;
+            SetMagicCircleAlpha(alpha);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    void SetMagicCircleAlpha(float alpha)
+    {
+        Color newColor = magicCircleRenderer.material.color;
+        newColor.a = alpha;
+        magicCircleRenderer.material.color = newColor;
+    }
 }
